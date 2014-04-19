de06fd1b49f859328a5baef84ac4cefe70cc3095
Christoph Jobst <cjobst@wifa.uni-leipzig.de>
2014-04-19 18:37:18 +0200

    Initial Commit

---
+++ ilias.ini.php
@@ -0,0 +1,63 @@
+; <?php exit; ?>
+[server]
+http_path = "http://localhost/ILIAS442"
+absolute_path = "C:/xampp/htdocs/ILIAS442"
+presetting = ""
+timezone = "Europe/Berlin"
+
+[clients]
+path = "data"
+inifile = "client.ini.php"
+datadir = "C:/xampp/iliasdata44"
+default = "iliasgit44"
+list = "0"
+
+[setup]
+pass = "6b34fe24ac2ff8103f6fce1f0da2ef57"
+
+[tools]
+convert = "C:/xampp/htdocs/ILIAS442/Services/Windows/bin32/convert/convert.exe"
+zip = "C:/xampp/htdocs/ILIAS442/Services/Windows/bin32/zip/zip.exe"
+unzip = "C:/xampp/htdocs/ILIAS442/Services/Windows/bin32/unzip/unzip.exe"
+java = ""
+htmldoc = ""
+ffmpeg = ""
+ghostscript = ""
+latex = ""
+vscantype = "none"
+scancommand = ""
+cleancommand = ""
+
+[log]
+path = "C:/xampp/iliasdata44"
+file = "logexternal.txt"
+enabled = "1"
+level = "WARNING"
+
+[debian]
+data_dir = "/var/opt/ilias"
+log = "/var/log/ilias/ilias.log"
+convert = "/usr/bin/convert"
+zip = "/usr/bin/zip"
+unzip = "/usr/bin/unzip"
+java = ""
+htmldoc = "/usr/bin/htmldoc"
+ffmpeg = "/usr/bin/ffmpeg"
+
+[redhat]
+data_dir = ""
+log = ""
+convert = ""
+zip = ""
+unzip = ""
+java = ""
+htmldoc = ""
+
+[suse]
+data_dir = ""
+log = ""
+convert = ""
+zip = ""
+unzip = ""
+java = ""
+htmldoc = ""
---
+++ include/inc.ilias_version.php
@@ -0,0 +1,14 @@
+<?php
+/* Copyright (c) 1998-2014 ILIAS open source e-Learning e.V., Extended GPL, see docs/LICENSE */
+
+/**
+* sets ilias version (this file shouldn't be merged between cvs branches)
+*
+* @author Alex Killing <alex.killing@gmx.de>
+* @version $Id: inc.ilias_version.php 49018 2014-03-26 14:01:34Z akill $
+*
+* @package ilias-core
+*/
+define("ILIAS_VERSION", "4.4.2 2014-03-26");
+define("ILIAS_VERSION_NUMERIC", "4.4.2");			// must be always x.y.z: x, y and z are numbers
+?>
